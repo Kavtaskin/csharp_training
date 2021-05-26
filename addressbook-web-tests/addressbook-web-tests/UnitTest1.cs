8318163ab7a96fbd5c437a9fb0bf1bef39628891
Skobachke <skobachke@gmail.com>
2021-05-27 01:08:23 +0300

    Project created

---
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addressbook_web_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
